7973ce8710d39f7e2412dc9f3b4eaef4d97e3581
Yami Medina <yamilethmedina1@gmail.com>
2016-09-16 16:04:52 -0400

    installed react-server

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
+// replace app component with instance of React Router
+// decides what components React needs to render when the URL changes
+// browserHistory tells React how to respond to a change in the URL - whenever the URL updates, React will interpret everything after the protocol (ex. http://www.blog.com -> /posts/5)
+import { Router, browserHistory } from 'react-router';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+  <Router history={browserHistory} />
   </Provider>
   , document.querySelector('.container'));
+
