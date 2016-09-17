7292e5f17c10cd80e49aea26b76632f4fa143457
Yami Medina <yamilethmedina1@gmail.com>
2016-09-17 19:18:10 -0400

    IndexRoutes

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,28 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostsIndex from './components/posts_index';
+
+const Greeting = () => {
+	return <div>hey there!</div>;
+};
+// nested route
+
+// if the route is / only matches the parent, show App (container, can have a header & footer etc.) and PostsIndex
 
 export default (
-<Route path="/" component={App} />
+<Route path="/" component={App}>
+	<IndexRoute component={PostsIndex} />
+	<Route path="greet" component={Greeting} />
+	<Route path="greet2" component={Greeting} />
+	<Route path="greet3" component={Greeting} />
+</Route>
 
 // ex. google.com/ => renders App
 );
+
+// current valid routes:
+// / 	App
+// /greet     App, Greeting
+// /greet2    App, Greeting
+// /greet3    App, Greeting
