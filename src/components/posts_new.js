509edb2b84d953302e2d8b6b353da171e4efc486
Yami Medina <yamilethmedina1@gmail.com>
2016-10-13 13:35:28 -0400

    refactored posts_new component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,8 +1,26 @@
+import _ from 'lodash';
 import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
+const FIELDS = {
+	title: {
+		type: 'input',
+		label: 'Title for Post'
+		// can add a custom validation feature for each field
+	},
+	categories: {
+		type: 'input',
+		label: 'Enter some categories'
+	},
+	content: {
+		type: 'textarea',
+		label: 'Post Contents'
+	}
+};
+
+// ['title', 'categories', 'content'];
 
 class PostsNew extends Component {
 
@@ -20,9 +38,25 @@ class PostsNew extends Component {
 				this.context.router.push('/');
 			});
 	}
+
+	// return an entire div 
+	renderField(fieldConfig, field) {
+		const fieldHelper = this.props.fields[field]; // from Redux Form object
+		
+		return(
+		<div className={`form-group ${fieldHelper.touched && fieldHelper.invalid ? 'has-danger' : ''}`}>
+				<label>{fieldConfig.label}</label>
+				<fieldConfig.type type="text" className="form-control" {...fieldHelper}/>
+					<div className="text-help">
+						{fieldHelper.touched ? fieldHelper.error : ''}
+				</div>
+		</div>
+		);
+
+	}
 	// define life cycle component
 	render() {
-		const { fields: { title, categories, content}, handleSubmit } = this.props;
+		const { handleSubmit } = this.props;
 		// equal to
 		// const handleSubmit = this.props.handleSubmit;
 		// const title = this.props.fields.title;
@@ -31,31 +65,14 @@ class PostsNew extends Component {
 			// when form is submitted, handleSubmit will be called with form contents. if form is valid, handleSubmit will call action creator (this.props.createPost) with the same form contents
 			// add has-danger class (from bootstrap) if field was touched and is invalid (can be made into a separate helper function)
 			// {`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}
-			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+			<form onSubmit={handleSubmit(props => this.onSubmit(props))}>
 				<h3>Create a New Post</h3>
-					<div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
-					<label>Title</label>
-					<input type="text" className="form-control" {...title}/>
-					<div className="text-help">
-						{title.touched ? title.error : ''}
-					</div>
-					</div>
-					<div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
-					<label>Categories</label>
-					<input type="text" className="form-control" {...categories} />
-					<div className="text-help">
-						{categories.touched ? categories.error : ''}
-					</div>
-					</div>
-					<div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
-					<label>Content</label>
-					<textarea type="text" className="form-control" {...content} />
-						<div className="text-help">
-						{content.touched ? content.error : ''}
-					</div>
+			
+				{ _.map(FIELDS, this.renderField.bind(this)) }
+
 					<button type="submit" className="btn btn-primary">Submit</button>
 					<Link to="/" className="btn btn-danger">Cancel</Link>
-					</div>
+					
 			</form>
 			);
 	}
@@ -69,17 +86,13 @@ function validate(values) {
 	// if the title does not exist, add a property to errors object called 'title' that contains the error text
 	// if the error object has a key that matches a field and that key has a truthy object tied to it, ReduxForm assumes the form is not valid and won't let you submit
 	// {title.error} shows the error in the render method
-	if (!values.title) {
-		errors.title = 'Enter a username';
-	}
+	_.each(FIELDS, (type, field) => {
 
-	if (!values.categories) {
-		errors.categories = "Enter categories";
-	}
+		if (!values[field]) {
+			errors[field] = `Enter ${field}`;
+		}
 
-	if (!values.content) {
-		errors.content = 'Enter some content';
-	}
+	});
 
 	return errors;
 
@@ -91,7 +104,7 @@ function validate(values) {
 export default reduxForm({
 	// name of form doesn't need to match the name of component, it only needs to be unique
 	form: 'PostsNewForm',
-	fields: ['title', 'categories', 'content'],
+	fields: _.keys(FIELDS), // returns an array of all the keys on the configuration object above
 	validate
 }, null, { createPost })(PostsNew);
 
